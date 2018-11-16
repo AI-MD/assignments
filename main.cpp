d683002852c9b1758d91e5b0f2f6396bbf6d1d21
visionNoob <insurgent92@gmail.com>
2018-11-16 16:37:32 +0900

    #1: Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
+using namespace cv;
 using namespace std;
 
 int main() {
-	cout << "hello world!" << endl;
+	Mat inputImage;
+	inputImage = imread("samples/sample.jpg");
+	imshow("sample", inputImage);
+	waitKey(0);
 	return 0;
 }
